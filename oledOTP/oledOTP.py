b00efe0a43c02aedfd483652bab4caf1b6fd9a75
Zheng Yang Tean <teanzhengyang@outlook.sg>
2017-09-17 19:53:45 +0800

    included door_id in qr code

--- oledOTP/oledOTP.py
+++ oledOTP/oledOTP.py
@@ -1,12 +1,13 @@
 import pyotp
 import oledQrCodeGenerator
 
+door_id="com1-1-13"
 totp = pyotp.TOTP('base32secret3232')
 lastPass = totp.now()
 
 while True:
     currPass = totp.now()
     if currPass != lastPass:
-        print("currently showing :", currPass)
-        oledQrCodeGenerator.dispQrCode(currPass)
+        print("currently showing :", door_id + "," + currPass)
+        oledQrCodeGenerator.dispQrCode( door_id + ',' + currPass)
     lastPass = currPass
