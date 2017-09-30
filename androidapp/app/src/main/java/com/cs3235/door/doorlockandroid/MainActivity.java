3ec8cbccc997e7ef3c21683aa79ccbdcf94b4adf
Tan Wang Leng <tanwangleng@outlook.com>
2017-09-30 14:43:27 +0800

    Add fingerprint scanning

--- androidapp/app/src/main/java/com/cs3235/door/doorlockandroid/MainActivity.java
+++ androidapp/app/src/main/java/com/cs3235/door/doorlockandroid/MainActivity.java
@@ -10,13 +10,18 @@ import android.widget.TextView;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import static com.cs3235.door.doorlockandroid.FingerprintActivity.RESULT_FINGERPRINT_NOT_RECOGNIZED;
+
 public class MainActivity extends AppCompatActivity {
 
     private static int LOGIN_REQUEST_CODE = 0x000000001;
+    private static int FINGER_PRINT_REQUEST_CODE = 0x000000002;
 
     private String currentUser = "";
     private String activatedDoorMessage = "";
     private String studentSecretKey = "studentSecretKey";
+
+    private String lastScannedQrCode = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
     public void onLoginClick(View view) {
         Intent login = new Intent(this, LoginActivity.class);
         startActivityForResult(login, LOGIN_REQUEST_CODE);
+    }
+
+    public void activateFingerprintActivity() {
+        Intent fingerprint = new Intent(this, FingerprintActivity.class);
+        startActivityForResult(fingerprint, FINGER_PRINT_REQUEST_CODE);
     }
 
     public void onScanClick(View view) {
@@ -68,7 +78,8 @@ public class MainActivity extends AppCompatActivity {
                 activatedDoorMessage = "";
             } else {
                 // we scanned something
-                activatedDoorMessage = "Welcome to " + result.getContents();
+                lastScannedQrCode = result.getContents();
+                activateFingerprintActivity();
             }
 
             updateMessageText();
@@ -82,6 +93,28 @@ public class MainActivity extends AppCompatActivity {
                 currentUser = loginDetails[0];
                 studentSecretKey = loginDetails[1];
                 updateMessageText();
+            }
+        }
+
+        // activity result received is fingerprint authentication
+        if (requestCode == FINGER_PRINT_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                // should send HTTP request to activate door
+                activatedDoorMessage = "Welcome to " + lastScannedQrCode;
+                updateMessageText();
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // the fingerprint scanning was abort by user
+                Snackbar mySnackbar = Snackbar.make(findViewById(R.id.mainActivity),
+                        "Fingerprint scanning failed: Authentication cancelled", Snackbar.LENGTH_SHORT);
+                mySnackbar.show();
+
+            } else if (resultCode == RESULT_FINGERPRINT_NOT_RECOGNIZED) {
+                // this is not user's fingerprints!
+                Snackbar mySnackbar = Snackbar.make(findViewById(R.id.mainActivity),
+                        "Fingerprint does not belong to user!", Snackbar.LENGTH_SHORT);
+                mySnackbar.show();
             }
         }
     }
